da88f05a4006bed3bb22c638f615f7dc7b3ca5ad
Zooheck <davidscottflack@gmail.com>
2019-01-11 12:53:01 -0600

    (Stretch) rearrange tile

--- starwars/src/components/CharacterProfile.js
+++ starwars/src/components/CharacterProfile.js
@@ -3,15 +3,15 @@ import './character-profile.css'
 const CharacterProfile = (props) => {
   return (
     <div className="character-profile">
-      <h2>{props.name}</h2>
-      <h3>Gender: {props.gender}</h3>
-      <h3>Birth year: {props.birth_year}</h3>
-      <h3>Height: {props.height}</h3>
-      <h3>Mass: {props.mass}</h3>
-      <div className="button-container">
-        <button onClick={props.togglePrevious}>Previous</button>
-        <button onClick={props.toggleNext}>Next</button>
+      <button onClick={props.togglePrevious}>Previous</button>
+      <div className="info-container">
+        <h2>{props.name}</h2>
+        <h3>Gender: {props.gender}</h3>
+        <h3>Birth year: {props.birth_year}</h3>
+        <h3>Height: {props.height}</h3>
+        <h3>Mass: {props.mass}</h3>      
       </div>
+      <button onClick={props.toggleNext}>Next</button>
     </div>
   )
 }
